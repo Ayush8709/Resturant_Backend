78497b7b49ab5a36a8150dcee5ecda9b8d77caaf
Ayush8709 <ayushsingh0455@gmail.com>
2024-04-24 01:21:34 -0700

    added jwt and bcrypt function

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,7 @@
 import userModel from "../models/userModel.js"
+import bcrypt  from 'bcryptjs'
+import JWT from 'jsonwebtoken'
+
 const authController = async (req, res) => {
     try {
         const { userName, email, password, phone, address } = req.body;
@@ -18,11 +21,14 @@ const authController = async (req, res) => {
             });
         }
 
+        //hashPassword
+        const hashPassword = await bcrypt.hash(password, 10)
+
         //create new user
         const user = await userModel.create({
             userName,
             email,
-            password,
+            password:hashPassword,
             address,
             phone
 
@@ -57,7 +63,7 @@ const loginController =async (req, res)=>{
         }
 
         //check user
-        const user = await userModel.findOne({email: email, password:password})
+        const user = await userModel.findOne({email: email})
         if(!user){
             return res.status(404).send({
                 success:false,
@@ -65,11 +71,26 @@ const loginController =async (req, res)=>{
             })
         }
 
+        // user password incrept and chek passwod
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(500).send({
+                success:false,
+                message:'Please enter valid email or password'
+            })
+        }
+
+        //create token 
+        const token = JWT.sign({id:user._id}, process.env.JWT_SECRET , {
+            expiresIn:"7d"
+        }) // user._id is coming from datbase id
+
         //if user login then send this message
         res.status(200).send({
             success:true,
             message:'Login successfull',
-            user
+            token,
+            // user
         })
     } catch (error) {
         console.log('Not Logiin')
