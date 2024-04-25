acfb94a60d9e0bff4bd4bcedb35c9dce62b550f0
Ayush8709 <ayushsingh0455@gmail.com>
2024-04-25 08:12:18 -0700

    added deleteCategory function

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,13 +1,23 @@
 import express from 'express'
 
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { createCategoryController } from '../controllers/categoryControllers.js';
+import { createCategoryController, getAllCategoryController, updateCatController, deleteCategoryController } from '../controllers/categoryControllers.js';
 const router = express()
 
 //Routes
 
 //Create Category
 router.post('/create',authMiddleware,createCategoryController )
+
+//Get All Category
+router.get('/getall', getAllCategoryController)
+
+//Updata Category
+router.put('/update/:id', authMiddleware , updateCatController)
+
+//Delete Route
+
+router.delete('/delete/:id', authMiddleware, deleteCategoryController)
 
 
 export default router;
