1d3709ebff9b4aba52f1562272ba07ae18d8dc04
Ayush8709 <ayushsingh0455@gmail.com>
2024-04-25 09:37:58 -0700

    added createFoodController , getAllFoodController, getSingleFoodControlelr, getsingleFoodByResturnController

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -51,4 +51,94 @@ const createFoodController = async (req, res)=>{
   }
 };
     
-export {createFoodController}
+
+// Get All Food Item
+
+  const getAllFoodControlelr = async (req,res)=>{
+    try {
+      const foods = await foodModal.find({});
+      if (!foods) {
+        return res.status(404).send({
+          success: false,
+          message: "no food items was found",
+        });
+      }
+      res.status(200).send({
+        success: true,
+        totalFoods: foods.length,
+        foods,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({
+        success: false,
+        message: "Erro In Get ALL Foods API",
+        error,
+      });
+    }
+  };
+
+    // Get Single Food with Id
+    const getSingleFoodController = async (req, res)=>{
+      try {
+        const foodId = req.params.id;
+        if (!foodId) {
+          return res.status(404).send({
+            success: false,
+            message: "please provide id",
+          });
+        }
+        const food = await foodModal.findById(foodId);
+        if (!food) {
+          return res.status(404).send({
+            success: false,
+            message: "No Food Found with htis id",
+          });
+        }
+        res.status(200).send({
+          success: true,
+          food,
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({
+          success: false,
+          message: "Error In get SIngle Food API",
+          error,
+        });
+      }
+    };
+
+    // Get Food by REsturant
+    const getFoodByResturantController = async (req, res) => {
+      try {
+        const resturantId = req.params.id;
+        if (!resturantId) {
+          return res.status(404).send({
+            success: false,
+            message: "please provide id",
+          });
+        }
+        const food = await foodModal.find({ resturnat: resturantId });
+        if (!food) {
+          return res.status(404).send({
+            success: false,
+            message: "No Food Found with htis id",
+          });
+        }
+        res.status(200).send({
+          success: true,
+          message: "food base on restuatrn",
+          food,
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({
+          success: false,
+          message: "Error In get SIngle Food API",
+          error,
+        });
+      }
+    };
+
+export {createFoodController , getAllFoodControlelr ,getSingleFoodController , getFoodByResturantController }
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -1,13 +1,22 @@
 import express from 'express'
 
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { createFoodController } from '../controllers/foodController.js';
+import { createFoodController, getAllFoodControlelr, getSingleFoodController , getFoodByResturantController } from '../controllers/foodController.js';
 const router = express()
 
 //Routes
 
 // Create Food
 router.post('/create', authMiddleware, createFoodController )
+
+//GET all Food
+router.get('/getall', getAllFoodControlelr )
+
+//Get Single Food with Id
+router.get('/get/:id', getSingleFoodController)
+
+//Get Food by Resturant
+router.get('/getresturant/:id', getFoodByResturantController)
 
 
 
